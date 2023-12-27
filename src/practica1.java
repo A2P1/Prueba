1711b1db79a1c1214f086444c10a903b1b912ba0
A2P1 <pabloazpitartegutierrez@gmail.com>
2023-12-27 14:39:49 +0100

    codigo principal leer fichero

---
+++ src/practica1.java
@@ -0,0 +1,20 @@
+package src;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class practica1 {
+
+    public static void main(String[] args) {
+        String nombreArchivo = "ruta_del_archivo.txt"; // Reemplaza con la ruta de tu archivo
+
+        try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        }
+    }
+}
